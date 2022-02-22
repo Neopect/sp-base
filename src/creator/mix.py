6b38ca628d0709a8344b4caeacbbb34dbca11f3f
Neopect <tyler.moen@earthlink.net>
2022-02-22 12:13:11 -0600

    update

--- src/creator/mix.py
+++ src/creator/mix.py
@@ -2,37 +2,45 @@ import random
 import math
 import dbCreator as db
 
-def exceptions():
-    pass
+# def exceptions():
+#     pass
 
 def dupCheck(plist):
     pass
 
-def randPlist(users, tracklim = 50, explicit = True):
+def explicit_check(plists):
+    # Explicit check
+    for plist in range(len(plists)):
+            for song in range(len(plist)):
+                if song[4] == "True":
+                    del plists[plist][song]
+    return plist
+
+def randPlist(users, prePlist, tracklim = 50, explicit = True):
     # Creates random playlist from set of users
     plists = []
     plist_final = []
 
-    # Append playlists from user
-    for x in users:
-        plists.append(db.fetchPlist(x))
+    # Either copy or append playlists from user
+    if prePlist != None or prePlist != []:
+        plists = prePlist
+    else:
+        for x in users:
+            plists.append(db.fetchPlist(x))
+
 
     #TODO Dup check
 
     # Explicit check
-    if(explicit == False):
-        for plist in range(len(plists)):
-            for song in range(len(plist)):
-                if song[4] == "True":
-                    del plists[plists][song]
-
+    if explicit == True:
+        plists = explicit_check(plists)
 
 
     # Take $tracklim out of each list at a time
     for x in range(len(plists)):
         random.shuffle(plists[x])
         try:
-            plist_final.append(plists[x[:tracklim]])
+            plist_final.append(plists[x][:tracklim])
         except:
             plist_final.append(plists[x])
 
@@ -42,18 +50,26 @@ def randPlist(users, tracklim = 50, explicit = True):
 
 
 
-def dayMix(users, tracklim = 50, days = 1, explicit = True):
-    pass
+def dayMix(users, prePlist, tracklim = 50, explicit = True, days = 2):
+    
+    plists, plist_sep, plists_mixed = []
 
-# def mixComb():
-#     pass
+    # Either copy or append playlists from user
+    if prePlist != None or prePlist != []:
+        plists = prePlist
+    else:
+        for x in users:
+            plists.append(db.fetchPlist(x))
 
-def dailyMix(plists, tracklim = 50, globs = list(), globlim = 50, days = 1, exceptions = False):
-    # Creates a daily mix playlist for a set number of days
-    plists_SP = []
-    plists_mixed = []
 
-    # Splits the playlists into sets of days
+    #TODO Dup check
+
+    # Explicit check
+    if explicit == True:
+        plists = explicit_check(plists)
+
+
+    # Splits the each playlist evenly between # of days into a group
     for plist in plists:
         random.shuffle(plist)
         length = len(plist)
@@ -64,14 +80,19 @@ def dailyMix(plists, tracklim = 50, globs = list(), globlim = 50, days = 1, exce
         while i < days-1:
             plist_tmp.append(plist[i*lengthD:i*lengthD+lengthD])
             i += 1
-        plists_SP.append(plists_SP)
+        plist_sep.append(plist_tmp)
 
+    # Combines each playlist into one
     i = 1
     while i < days:
         plist_tmp = []
-        for plist in plists:
-            plist_tmp.append(plist[i-1])
-        plists_mixed.append(randPlist(plist_tmp, tracklim, globs, globlim))
+        for plist in range(len(plists)):
+            try:
+                plist_tmp.append(plist_sep[plist][i-1][:tracklim])
+            except:
+                plist_tmp.append(plist_sep[plist][i-1])
+        
+        plists_mixed.append(plist_tmp)
         i += 1
 
     return plists_mixed
